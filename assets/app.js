a3a428e17b3fcf4bc39195fc90610b9282e8ed36
Venomous0511 <johnrenierbaldeviso2@gmail.com>
2023-10-13 02:44:46 +0800

    Signed-off-by: Venomous0511 <johnrenierbaldeviso2@gmail.com>

--- assets/app.js
+++ assets/app.js
@@ -1,64 +1,47 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
 const app = express();
-const port = 5500; // Set your desired port number
-const uri = "mongodb+srv://Venomous:dj0tQFcISI1z8ENC@reservation.enj4dcj.mongodb.net/?retryWrites=true&w=majority";
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/submit', async (req, res) => {
-    res.sendFile(__dirname + 'services.html');
+mongoose.connect("mongodb+srv://Venomous:dj0tQFcISI1z8ENC@reservation.enj4dcj.mongodb.net/Reservation");
 
+const reservationSchema = {
+    name: String,
+    email: String,
+    address: String,
+    date: Date,
+    cottage: String,
+    kids: String,
+    adults: String
+}
+
+const reserve = mongoose.model("DayReservation", reservationSchema);
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/services.html");
+})
+
+app.post("/", async function (req, res) {
     try {
-        const client = new MongoClient(uri, {
-            serverApi: {
-                version: ServerApiVersion.v1,
-                strict: true,
-                deprecationErrors: true,
-            }
+        let newreserve = new reserve({
+            nameday: req.body.name,
+            emailday: req.body.emailday,
+            addressday: req.body.addressday,
+            dateday: req.body.dateday,
+            cottages: req.body.cottages,
+            kidsday: req.body.kidsday,
+            adultsday: req.body.adultsday
         });
 
-        await client.connect();
-
-        const dataschema = new Schema({
-            name: String,
-            email: String,
-            address: String,
-            date: Date,
-            cottage: String,
-            kids: int,
-            adults: int
-        })
-
-        const Data = mongoose.model('Data', dataSchema);
-
-        const reservationData = {
-            name: req.body['name-day'],
-            email: req.body['email-day'],
-            address: req.body['address-day'],
-            date: req.body['date-day'],
-            cottage: req.body['cottage'],
-            kids: req.body['kids-day'],
-            adults: req.body['adults-day']
-        };
-
-        const database = client.db('Reservation'); // Replace 'your-database-name' with your actual database name
-        const collection = database.collection('DayReservation'); // Replace 'reservations' with your desired collection name
-
-        await collection.insertOne(reservationData);
-        console.log('Reservation data inserted into the database.');
-
-        res.send('Reservation successful!'); // Send a response to the client
+        await newreserve.save();
+        res.status(200).send("Data saved successfully");
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    } finally {
-        await client.close();
+        res.status(500).send("Error saving data: " + error.message);
     }
-});
+})
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.listen(3000, function () {
+    console.log("server is running on 3000");
+})

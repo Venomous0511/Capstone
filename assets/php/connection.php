0d123d25b97499c4e9a63f745a390773257f98b8
Venomous0511 <johnrenierbaldeviso2@gmail.com>
2023-10-18 00:10:07 +0800

    Signed-off-by: Venomous0511 <johnrenierbaldeviso2@gmail.com>

--- assets/php/connection.php
+++ assets/php/connection.php
@@ -13,27 +13,60 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nameday = $_POST["nameday"];
-    $emailday = $_POST["emailday"];
-    $addressday = $_POST["addressday"];
-    $dateday = $_POST["dateday"];
-    $cottage_id = $_POST["cottages"];
-    $kidsday = $_POST["kidsday"];
-    $adultsday = $_POST["adultsday"];
+    if(isset($_POST["nameday"])) {
+        // Day Reservation form submitted
+        $nameday = $_POST["nameday"];
+        $emailday = $_POST["emailday"];
+        $addressday = $_POST["addressday"];
+        $dateday = $_POST["dateday"];
+        $cottage_id = $_POST["cottages"];
+        $kidsday = $_POST["kidsday"];
+        $adultsday = $_POST["adultsday"];
 
-    // Prepare and execute SQL query
-    $sql = "INSERT INTO day_reservations (nameday, emailday, addressday, dateday, cottage_id, kidsday, adultsday)
-            VALUES ('$nameday', '$emailday', '$addressday', '$dateday', $cottage_id, $kidsday, $adultsday)";
+        // Prepare and execute SQL query
+        $sql = "INSERT INTO day_reservations (nameday, emailday, addressday, dateday, cottage_id, kidsday, adultsday)
+                VALUES (?, ?, ?, ?, ?, ?, ?)";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Reservation successful!";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssssiii", $nameday, $emailday, $addressday, $dateday, $cottage_id, $kidsday, $adultsday);
+
+        if ($stmt->execute()) {
+            echo "Day Reservation successful!";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+        
+        $stmt->close();
+    } elseif(isset($_POST["name-night"])) {
+        // Night Reservation form submitted
+        $nameNight = $_POST["name-night"];
+        $emailNight = $_POST["email-night"];
+        $addressNight = $_POST["address-night"];
+        $dateNight = $_POST["date-night"];
+        $roomNumber = $_POST["rooms"];
+        $kidsNight = $_POST["kids-night"];
+        $adultsNight = $_POST["adults-night"];
+
+        // Prepare and execute SQL query
+        $sql = "INSERT INTO night_reservations (nameNight, emailNight, addressNight, dateNight, roomnumber, kidsNight, adultsNight)
+                VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssssiii", $nameNight, $emailNight, $addressNight, $dateNight, $roomNumber, $kidsNight, $adultsNight);
+
+        if ($stmt->execute()) {
+            echo "Night Reservation successful!";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+        
+        $stmt->close();
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Invalid form submission.";
     }
-
-    // Close connection
-    $conn->close();
 }
+
+// Close connection
+$conn->close();
 ?>

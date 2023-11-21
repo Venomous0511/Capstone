0c6e5260fbd8e4e5d2f2689d2f13f3fb2432dcfb
Venomous0511 <johnrenierbaldeviso2@gmail.com>
2023-11-22 01:22:10 +0800

    Signed-off-by: Venomous0511 <johnrenierbaldeviso2@gmail.com>

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -10,3 +10,29 @@ openMenu.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     mainMenu.style.top = '-100%';
 });
+
+(function () {
+    emailjs.init("fxrehaacDoVAy464Y");
+})();
+
+function SendMail() {
+    let messageValue = document.getElementById("message").value;
+
+    if (messageValue.trim() !== "") {
+        let params = {
+            full_name: document.getElementById("full_name").value,
+            email_id: document.getElementById("email_id").value,
+            message: messageValue
+        };
+
+        emailjs.send("service_ueink36", "template_doprjrk", params)
+            .then(function (res) {
+                alert("Message Sent");
+            })
+            .catch(function (error) {
+                alert("Error sending message: " + error);
+            });
+    } else {
+        alert("Message is empty. Please enter a message before sending.");
+    }
+}

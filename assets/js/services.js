c864268fd5314f191e58eeefa73ee9979259a37c
Venomous0511 <johnrenierbaldeviso2@gmail.com>
2023-12-16 22:11:07 +0800

    Signed-off-by: Venomous0511 <johnrenierbaldeviso2@gmail.com>

--- assets/js/services.js
+++ assets/js/services.js
@@ -14,14 +14,17 @@ closeMenu.addEventListener('click', () => {
 // Get modal elements
 const modal1 = document.getElementById('reservationModal1');
 const modal2 = document.getElementById('reservationModal2');
+const modal3 = document.getElementById('reservationModal3');
 
 // Get close buttons
 const closeModal1 = document.getElementById('closeModal1');
 const closeModal2 = document.getElementById('closeModal2');
+const closeModal3 = document.getElementById('closeModal3');
 
 // Get reservation buttons
 const reservationBtn1 = document.getElementById('sunBtn');
 const reservationBtn2 = document.getElementById('moonBtn');
+const reservationBtn3 = document.getElementById('payment');
 
 // Function to open modal
 function openModal(modal) {
@@ -36,10 +39,12 @@ function closeModal(modal) {
 // Event listeners to open modals
 reservationBtn1.addEventListener('click', () => openModal(modal1));
 reservationBtn2.addEventListener('click', () => openModal(modal2));
+reservationBtn3.addEventListener('click', () => openModal(modal3));
 
 // Event listeners to close modals
 closeModal1.addEventListener('click', () => closeModal(modal1));
 closeModal2.addEventListener('click', () => closeModal(modal2));
+closeModal3.addEventListener('click', () => closeModal(modal3));
 
 // Event listener to close modals when clicking outside
 window.addEventListener('click', (event) => {
@@ -48,5 +53,8 @@ window.addEventListener('click', (event) => {
     }
     if (event.target === modal2) {
         closeModal(modal2);
+    }
+    if (event.target === modal3) {
+        closeModal(modal3);
     }
 });

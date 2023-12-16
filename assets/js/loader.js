d9793004138ca6793a4fbd0e3e03e307142eac1c
Venomous0511 <johnrenierbaldeviso2@gmail.com>
2023-12-16 22:34:57 +0800

    Signed-off-by: Venomous0511 <johnrenierbaldeviso2@gmail.com>

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -7,3 +7,20 @@ window.onload = function () {
         loader.style.display = "none";
     });
 };
+
+const goToTopBtn = document.getElementById("gotoTop");
+
+// Show or hide the button based on scroll position
+window.onscroll = function () {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 20) {
+        goToTopBtn.style.display = "block";
+    } else {
+        goToTopBtn.style.display = "none";
+    }
+};
+
+// Scroll to the top when the button is clicked
+goToTopBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
